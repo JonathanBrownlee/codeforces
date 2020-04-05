84e1bc40813e81b488f5e2e021d3f294aa634b43
Jonathan Brownlee <jon.brownlee@hotmail.com>
2020-04-05 20:44:09 +1000

    Updated template

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 let lines = [];
 require('readline').createInterface({input: process.stdin,output: process.stdout})
 				.on('line', line => {lines.push(line);})
-				.on('close', () => {console.log(main(lines));});
+				.on('close', () => {main(lines)});
 
 const main = (input) => {
 	// Code goes here
